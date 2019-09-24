88f38c2417044da13bc5ffbc63756ed402e0b1d6
cgoez <goez2001@gmail.com>
2019-09-24 14:32:47 -0700

    Fixed nav routes for homepage, settings

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,22 +1,58 @@
-import React, {useContext} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import UserContext from "../../context/userContext";
-import './Navbar.scss'
+import "./Navbar.scss";
 
 const Navbar = () => {
-    const userContext = useContext(UserContext);
-    const { logout, setRoute } = userContext;
+  const userContext = useContext(UserContext);
+  const { logout, setRoute } = userContext;
 
-    return (
-        <div className="nav-container">
-            <div className="nav-items-container" >
-                <Link style={{ textDecoration: 'none', marginRight: '20px', color: 'black' }} to="/homepage">Home</Link>
-                <Link style={{ textDecoration: 'none', marginRight: '20px', color: 'black' }} onClick={() => setRoute("forum")}>Forum</Link>
-                <Link style={{ textDecoration: 'none', marginRight: '20px', color: 'black' }} to="/settings">Settings</Link>
-                <Link style={{ textDecoration: 'none', marginRight: '20px', color: 'black' }} onClick={logout}>Sign Out</Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="nav-container">
+      <div className="nav-items-container">
+        <Link
+          style={{
+            textDecoration: "none",
+            marginRight: "20px",
+            color: "black"
+          }}
+          onClick={() => setRoute("homepage")}
+        >
+          Home
+        </Link>
+        <Link
+          style={{
+            textDecoration: "none",
+            marginRight: "20px",
+            color: "black"
+          }}
+          onClick={() => setRoute("forum")}
+        >
+          Forum
+        </Link>
+        <Link
+          style={{
+            textDecoration: "none",
+            marginRight: "20px",
+            color: "black"
+          }}
+          onClick={() => setRoute("settings")}
+        >
+          Settings
+        </Link>
+        <Link
+          style={{
+            textDecoration: "none",
+            marginRight: "20px",
+            color: "black"
+          }}
+          onClick={logout}
+        >
+          Sign Out
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
