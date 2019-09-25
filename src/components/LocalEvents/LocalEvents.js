bcf3082d282e1b0e432912cc6975e498f285ac1b
James-AR <ruiz.james12@yahoo.com>
2019-09-24 21:10:28 -0700

    Start on CSS Local Events

--- src/components/LocalEvents/LocalEvents.js
+++ src/components/LocalEvents/LocalEvents.js
@@ -19,21 +19,24 @@ const LocalEvents = () => {
     const { loading, error, data } = useQuery(GET_EVENTS);
 
     return (
-
-        <div className="local-events">
-            <h3 className="events-message">
-              WHAT'S HAPPENING NEAR <br/><h2 className="events-city-name">LOS ANGELES?</h2>
-            </h3>
-            <h3 className="events-upcoming-title">UPCOMING EVENTS</h3><hr className="line" />
-            {loading ? <h1>Loading...</h1> : data.events.map(event => (
-                <div>
-                    <div className="events-title">
+        <div className="local-events-container">
+          <div className="local-left-container">
+            <div>
+              <h3>Test</h3>
+            </div>
+            <div className="local-events">
+                {loading ? <h1>Loading...</h1> : data.events.map(event => (
+                    <div className="events-container">
                         <p>{event.title}</p>
                         <p>{event.genre}</p>
+                        <p>{event.description}</p>
                     </div>
-                    <p className="event-description">{event.description}</p>
-                </div>
-            ))}
+                ))}
+            </div>
+          </div>
+          <div>
+            <button className="button-local">CREATE EVENT</button>
+          </div>
         </div>
     )
 }
