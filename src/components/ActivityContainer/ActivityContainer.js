414a84f61c3a0df65271f5a8f950c0d8dcb57001
cgoez <goez2001@gmail.com>
2019-09-30 18:19:41 -0700

    Reworked LocalEvents positioning, styling

--- src/components/ActivityContainer/ActivityContainer.js
+++ src/components/ActivityContainer/ActivityContainer.js
@@ -1,35 +1,30 @@
-import React, {useState, useContext} from 'react'
+import React, { useContext } from "react";
 
-import "./ActivityContainer.scss"
-import Dashboard from '../Dashboard/Dashboard'
-import LocalEvents from '../LocalEvents/LocalEvents'
-import CreateEvent from '../CreateEvent/CreateEvent'
+import "./ActivityContainer.scss";
+import Dashboard from "../Dashboard/Dashboard";
+import LocalEvents from "../LocalEvents/LocalEvents";
+import CreateEvent from "../CreateEvent/CreateEvent";
 import UserContext from "../../context/userContext";
-import Forum from '../Forum/Forum'
-import Settings from '../Settings/Settings'
-import CreatePost from '../CreatePost/CreatePost'
+import Forum from "../Forum/Forum";
+import Settings from "../Settings/Settings";
+import CreatePost from "../CreatePost/CreatePost";
 
 const ActivityContainer = () => {
-    const userContext = useContext(UserContext);
-    console.log(userContext)
-    const route = userContext.userState.route
-    console.log('route', route)
+  const userContext = useContext(UserContext);
+  console.log(userContext);
+  const route = userContext.userState.route;
+  console.log("route", route);
 
-    return (
-        <div className="activity-container">
-            <Dashboard />
-            {route === "homepage" ? (
-            <LocalEvents />) : "" }
-            {route === "createEvent" ? (
-            <CreateEvent />) : ""}
-            {route === "forum" ? (
-            <Forum />) : ""}
-            {route === "createPost" ? (
-            <CreatePost />) : ""}
-            {route === "settings" ? (
-            <Settings/>) : ""}
-        </div>
-    )
-}
+  return (
+    <div className="activity-container">
+      <Dashboard />
+      {route === "homepage" ? <LocalEvents /> : ""}
+      {route === "createEvent" ? <CreateEvent /> : ""}
+      {route === "forum" ? <Forum /> : ""}
+      {route === "createPost" ? <CreatePost /> : ""}
+      {route === "settings" ? <Settings /> : ""}
+    </div>
+  );
+};
 
-export default ActivityContainer
+export default ActivityContainer;
--- src/components/LocalEvents/LocalEvents.js
+++ src/components/LocalEvents/LocalEvents.js
@@ -1,52 +1,62 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 
-import "./LocalEvents.scss"
-import UserContext from "../../context/userContext"
+import "./LocalEvents.scss";
+import UserContext from "../../context/userContext";
 
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 
 const LocalEvents = () => {
-    const userContext = useContext(UserContext)
-    const { setRoute } = userContext
+  const userContext = useContext(UserContext);
+  const { setRoute } = userContext;
 
-    const GET_EVENTS = gql`
+  const GET_EVENTS = gql`
     {
       events {
         title
         description
         genre
       }
-    }`;
+    }
+  `;
 
-    const { loading, error, data } = useQuery(GET_EVENTS);
+  const { loading, error, data } = useQuery(GET_EVENTS);
 
-    return (
-        <div className="local-events-container">
-          <div className="local-left-container">
-            <div className="local-headline">
-              <h2>WHAT'S HAPPENING NEAR </h2> 
-              <br /> 
-              <h1 className="local-your-city">LOS ANGELES?</h1>
-              <h3 className="local-upcoming">UPCOMING EVENTS</h3>
-            </div>
-            <div className="local-events">
-                {loading ? <h1>Loading...</h1> : data.events.map(event => (
-                    <div className="events-container">
-                        <p>{event.title}</p>
-                        <p>{event.genre}</p>
-                        <p>{event.description}</p>
-                    </div>
-                ))}
-            </div>
-          </div>
-          <div className="local-right-container">
-            <div className="button">
-              <button onClick={() => setRoute("createEvent")} className="local-button">CREATE EVENT</button>
-            </div>
-          </div>
+  return (
+    <div className="localEvents__container">
+      <div className="localEvents__header">
+        <div>
+          <h3 className="localEvents__header--text">WHAT'S HAPPENING NEAR </h3>
+          <span className="localEvents__header--city">LOS ANGELES?</span>
         </div>
-    )
-}
+        {/* <div> */}
+        <button
+          onClick={() => setRoute("createEvent")}
+          className="localEvents__header--button"
+        >
+          CREATE EVENT
+        </button>
+        {/* </div> */}
+      </div>
+      <div className="localEvents__upcomingEvents">
+        <h3 className="localEvents__upcomingEvents--header">UPCOMING EVENTS</h3>
+        <div className="localEvents__upcomingEvents--events">
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : (
+            data.events.map(event => (
+              <div className="events-container">
+                <p>{event.title}</p>
+                <p>{event.genre}</p>
+                <p>{event.description}</p>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+      <div className="localEvents__recentDiscussions"></div>
+    </div>
+  );
+};
 
-export default LocalEvents
+export default LocalEvents;
