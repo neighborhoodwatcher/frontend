8ea57f9311b409728a9c0639a9b2f5372e07ea60
James <ruiz.james12@yahoo.com>
2019-09-30 17:08:21 -0700

    Create Post Mutation

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -6,6 +6,32 @@ import UserContext from '../../context/userContext'
 import './CreatePost.scss'
 
 const CreatePost = () => {
+    const [titleState, setTitleState] = useState('')
+    const [bodyState, setBodyState] = useState('')
+    const [topicState, setTopicState] = useState('')
+    const userContext = useContext(UserContext)
+
+    const CREATE_POST = gql`
+        mutation createPost ($title: String!, $body: String!, $topic: String!, $user_id: String!) {
+            insert_posts(objects: {title: $title, body: $body, topic: $topic, user_id: $user_id}) {
+                affected_rows
+        }
+    }`
+
+    const [insert_posts] = useMutation(CREATE_POST)
+    const uid = userContext.userState.user.uid
+
+    const createPost = (insert_posts, titleState, bodyState, topicState, uid) => {
+        if(titleState && bodyState && topicState) {
+            insert_posts({variables:{title: titleState, body: bodyState, topic: topicState, user_id: uid}})
+        } else {
+            alert('You must fill in everything punk')
+        }
+        setTitleState('')
+        setBodyState('')
+        setTopicState('')
+    }
+
     return (
         <div className="post-container">
             <div className="post-header">
@@ -17,22 +43,31 @@ const CreatePost = () => {
                 <div className="post-left-container">
                     <div>
                         <p>Title</p>
-                        <input className="post-title" />
+                        <input 
+                        onChange={e => setTitleState(e.target.value)} 
+                        className="post-title"
+                        value={titleState} />
                     </div>
                     <div>
                         <p>Body</p>
-                        <textarea className="post-body" />
+                        <textarea 
+                        onChange={e => setBodyState(e.target.value)} 
+                        className="post-body"
+                        value={bodyState} />
                     </div>
                 </div>
                 <div className="post-right-container">
                     <div>
                         <p>Topic</p>
-                        <select className="post-select">
+                        <select onClick={e => setTopicState(e.target.value)} className="post-select">
                             <option>Pick One</option>
                             <option value="Fitness">Fitness</option>
+                            <option value="Recreational">Recreational</option>
+                            <option value="Sport">Sport</option>
+                            <option value="Music">Music</option>
                         </select>
-
                     </div>
+                    <button onClick={() => createPost(insert_posts, titleState, bodyState, topicState, uid)}>Submit</button>
                 </div>
             </div>
         </div>
