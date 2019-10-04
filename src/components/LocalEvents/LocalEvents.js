ad1d8110ecf46a1f1dc82a1ead8cff7d14c5306c
cgoez <goez2001@gmail.com>
2019-10-04 13:03:32 -0700

    LocalEvents updated styling on events

--- src/components/LocalEvents/LocalEvents.js
+++ src/components/LocalEvents/LocalEvents.js
@@ -38,23 +38,48 @@ const LocalEvents = () => {
         </button>
         {/* </div> */}
       </div>
-      <div className="localEvents__upcomingEvents">
-        <h3 className="localEvents__upcomingEvents--header">UPCOMING EVENTS</h3>
-        <div className="localEvents__upcomingEvents--events">
-          {loading ? (
-            <h1>Loading...</h1>
-          ) : (
-            data.events.map(event => (
-              <div className="events-container">
-                <p>{event.title}</p>
-                <p>{event.topic}</p>
-                <p>{event.description}</p>
-              </div>
-            ))
-          )}
+      <div className="localEvents__container--bottom">
+        <div className="localEvents__upcomingEvents">
+          <h3 className="localEvents__upcomingEvents--header">
+            UPCOMING EVENTS
+          </h3>
+          <div className="localEvents__upcomingEvents--events">
+            {loading ? (
+              <h1>Loading...</h1>
+            ) : (
+              data.events.map(event => (
+                <div className="localEvents__event">
+                  <img
+                    src="https://api.adorable.io/avatars/285/abott@adorable.png"
+                    alt="User Avatar"
+                    className="localEvents__event--image"
+                  />
+                  <div className="localEvents__event--title">{event.title}</div>
+                  <div className="localEvents__event--info">
+                    <div className="localEvents__event--infoLocation">
+                      LOCATION
+                    </div>
+                    <div className="localEvents__event--infoDate">DATE</div>
+                  </div>
+
+                  <div className="localEvents__event--description">
+                    {event.description}
+                  </div>
+                  <div className="localEvents__event--topic">
+                    #{event.topic}
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+        <div className="localEvents__recentDiscussions">
+          <h3 className="localEvents__recentDiscussions--header">
+            RECENT DISCUSSIONS
+          </h3>
+          <div className="localEvents__recentDiscussions--discussions"></div>
         </div>
       </div>
-      <div className="localEvents__recentDiscussions"></div>
     </div>
   );
 };
